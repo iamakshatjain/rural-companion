93fd6d5592c4a7b9933b1cb2578351dd4c01c8bf
Kristency <nandisubhajit666@gmail.com>
2020-03-03 23:41:21 +0530

    add cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import { Redirect, Route } from 'react-router-dom'
-import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react'
-import { IonReactRouter } from '@ionic/react-router'
-import { ellipse, square, triangle } from 'ionicons/icons'
-import Tab1 from './pages/Tab1'
-import Tab2 from './pages/Tab2'
-import Tab3 from './pages/Tab3'
+import {
+	IonButton,
+	IonCard,
+	IonCardContent,
+	IonCardHeader,
+	IonCardSubtitle,
+	IonCardTitle,
+	IonContent,
+	IonIcon,
+	IonItem,
+	IonLabel
+} from '@ionic/react'
+// import { Redirect, Route } from 'react-router-dom'
+import { IonApp, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react'
+// import { IonReactRouter } from '@ionic/react-router'
+// import { ellipse, square, triangle } from 'ionicons/icons'
+// import Tab1 from './pages/Tab1'
+// import Tab2 from './pages/Tab2'
+// import Tab3 from './pages/Tab3'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css'
@@ -27,31 +39,68 @@ import '@ionic/react/css/display.css'
 import './theme/variables.css'
 
 const App = () => (
+	// <IonApp>
+	// 	<IonReactRouter>
+	// 		<IonTabs>
+	// 			<IonRouterOutlet>
+	// 				<Route path="/tab1" component={Tab1} exact={true} />
+	// 				<Route path="/tab2" component={Tab2} exact={true} />
+	// 				<Route path="/tab3" component={Tab3} />
+	// 				<Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
+	// 			</IonRouterOutlet>
+	// 			<IonTabBar slot="bottom">
+	// 				<IonTabButton tab="tab1" href="/tab1">
+	// 					<IonIcon icon={triangle} />
+	// 					<IonLabel>Tab 1</IonLabel>
+	// 				</IonTabButton>
+	// 				<IonTabButton tab="tab2" href="/tab2">
+	// 					<IonIcon icon={ellipse} />
+	// 					<IonLabel>Tab 2</IonLabel>
+	// 				</IonTabButton>
+	// 				<IonTabButton tab="tab3" href="/tab3">
+	// 					<IonIcon icon={square} />
+	// 					<IonLabel>Tab 3</IonLabel>
+	// 				</IonTabButton>
+	// 			</IonTabBar>
+	// 		</IonTabs>
+	// 	</IonReactRouter>
+	// </IonApp>
 	<IonApp>
-		<IonReactRouter>
-			<IonTabs>
-				<IonRouterOutlet>
-					<Route path="/tab1" component={Tab1} exact={true} />
-					<Route path="/tab2" component={Tab2} exact={true} />
-					<Route path="/tab3" component={Tab3} />
-					<Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
-				</IonRouterOutlet>
-				<IonTabBar slot="bottom">
-					<IonTabButton tab="tab1" href="/tab1">
-						<IonIcon icon={triangle} />
-						<IonLabel>Tab 1</IonLabel>
-					</IonTabButton>
-					<IonTabButton tab="tab2" href="/tab2">
-						<IonIcon icon={ellipse} />
-						<IonLabel>Tab 2</IonLabel>
-					</IonTabButton>
-					<IonTabButton tab="tab3" href="/tab3">
-						<IonIcon icon={square} />
-						<IonLabel>Tab 3</IonLabel>
-					</IonTabButton>
-				</IonTabBar>
-			</IonTabs>
-		</IonReactRouter>
+		<IonContent>
+			<IonCard>
+				<IonCardHeader>
+					<IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+					<IonCardTitle>Card Title</IonCardTitle>
+				</IonCardHeader>
+
+				<IonCardContent>
+					Keep close to Nature's heart... and break clear away, once in awhile, and climb a mountain or spend a week in the woods.
+					Wash your spirit clean.
+				</IonCardContent>
+			</IonCard>
+			<IonCard>
+				<IonCardHeader>
+					<IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+					<IonCardTitle>Card Title</IonCardTitle>
+				</IonCardHeader>
+
+				<IonCardContent>
+					Keep close to Nature's heart... and break clear away, once in awhile, and climb a mountain or spend a week in the woods.
+					Wash your spirit clean.
+				</IonCardContent>
+			</IonCard>
+			<IonCard>
+				<IonCardHeader>
+					<IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+					<IonCardTitle>Card Title</IonCardTitle>
+				</IonCardHeader>
+
+				<IonCardContent>
+					Keep close to Nature's heart... and break clear away, once in awhile, and climb a mountain or spend a week in the woods.
+					Wash your spirit clean.
+				</IonCardContent>
+			</IonCard>
+		</IonContent>
 	</IonApp>
 )
 
