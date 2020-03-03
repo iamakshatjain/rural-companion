009e1091ba3f45269681ec032141942a3669dacb
Kristency <nandisubhajit666@gmail.com>
2020-03-03 23:03:32 +0530

    change ts to js

---
+++ src/components/ExploreContainer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './ExploreContainer.css'
+
+const ExploreContainer = ({ name }) => {
+	return <div className="container">Hi</div>
+}
+
+export default ExploreContainer
