bd925eb3dcf92b0e9a087fed0852aae74f0da31b
iamakshatjain <jain99akshat@gmail.com>
2020-06-22 00:31:19 +0530

    feat: covid 19

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import YouTubePlayer from 'react-player/lib/players/YouTube';
+import React from "react";
+import YouTubePlayer from "react-player/lib/players/YouTube";
 
-import '../assets/stylesheets/VideoPlayer.css';
+import "../static-data/assets/stylesheets/VideoPlayer.css";
 
-const VideoPlayer = ({url}) => {
-
-	return (
-		<YouTubePlayer className="react-player"
-			width="100%" 
-			height="100%" 
-			url={url} 
-			playing controls 	
-		/>
-	);
+const VideoPlayer = ({ url }) => {
+  return (
+    <YouTubePlayer
+      className="react-player"
+      width="100%"
+      height="100%"
+      url={url}
+      playing
+      controls
+    />
+  );
 };
 
 export default VideoPlayer;
--- src/static-data/cards.js
+++ src/static-data/cards.js
@@ -1,39 +1,69 @@
-import Woman from '../assets/images/woman.svg'
-import Child from '../assets/images/child.svg'
-import Cattle from '../assets/images/cattle.svg'
-import Medicine from '../assets/images/medicine.svg'
-import Vegetables from '../assets/images/clubbed_veg.svg'
-import Soap from '../assets/images/soap.svg'
+import Woman from "./assets/images/woman.svg";
+import Child from "./assets/images/child.svg";
+import Cattle from "./assets/images/cattle.svg";
+import Vaccine from "./assets/images/vaccine.svg";
+import Vegetables from "./assets/images/clubbed_veg.svg";
+import Soap from "./assets/images/soap.svg";
+import Virus from "./assets/images/virus.svg";
+import Symptoms from "./assets/images/symptoms.svg";
+import Handwashing from "./assets/images/handwashing.svg";
+import Outdoor from "./assets/images/outdoor.svg";
+import OutdoorToIndoor from "./assets/images/outdoorToIndoor.svg";
+import Indoor from "./assets/images/indoor.svg";
 
 export const categoryCards = [
-	{
-		img: Woman,
-		desc: 'women'
-	},
-	{
-		img: Child,
-		desc: 'children',
-		route: '/'
-	},
-	{
-		img: Cattle,
-		desc: 'cattle',
-		route: '/'
-	}
-]
+  {
+    img: Woman,
+    desc: "women",
+  },
+  {
+    img: Child,
+    desc: "children",
+  },
+  {
+    img: Cattle,
+    desc: "cattle",
+  },
+  {
+    img: Virus,
+    desc: "virus",
+  },
+];
 
-export const informationCards = [
-	{
-		img: Medicine,
-		desc: 'disease',
-		route: ''
-	},
-	{
-		img: Vegetables,
-		desc: 'nutrition'
-	},
-	{
-		img: Soap,
-		desc: 'hygiene'
-	}
-]
+export const subCategoryCards = [
+  {
+    img: Vaccine,
+    desc: "disease",
+  },
+  {
+    img: Vegetables,
+    desc: "nutrition",
+  },
+  {
+    img: Soap,
+    desc: "hygiene",
+  },
+];
+
+export const virusCards = [
+  {
+    img: Symptoms,
+    desc: "symptoms",
+  },
+  {
+    img: Handwashing,
+    desc: "handwashing",
+  },
+  {
+    img: Outdoor,
+    desc: "outdoor",
+  },
+  {
+    img: OutdoorToIndoor,
+    desc: "outdoorToIndoor",
+  },
+  {
+    img: Indoor,
+    desc: "indoor",
+  },
+];
