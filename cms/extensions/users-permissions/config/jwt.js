ce75ab0ad3cfff9bbcfbc541cad9fb55a99fdcbd
iamakshatjain <jain99akshat@gmail.com>
2020-07-20 00:25:12 +0530

    feat: cms

---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4b32e34c-09d3-4f15-abac-ce9956ac2100'
+};
--- src/components/ExploreContainer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import '../assets/stylesheets/ExploreContainer.css';
-const ExploreContainer = () => {
-	return <div>Hi</div>;
-};
-
-export default ExploreContainer;
