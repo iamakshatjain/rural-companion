fcc524f25eb65818b67479918da635adc9b4a0f2
iamakshatjain <jain99akshat@gmail.com>
2020-07-27 03:29:35 +0530

    fix: content builder types

--- cms/config/database.js
+++ cms/config/database.js
@@ -4,7 +4,8 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        uri: env("DATABASE_URI"),
+        uri:
+          "mongodb://admin_rc:admin_rc@gramin-mitra-shard-00-00.tfksc.mongodb.net:27017,gramin-mitra-shard-00-01.tfksc.mongodb.net:27017,gramin-mitra-shard-00-02.tfksc.mongodb.net:27017/gramin-mitra?ssl=true&replicaSet=atlas-ks6zw1-shard-0&authSource=admin&retryWrites=true&w=majority",
       },
       options: {
         ssl: true,
