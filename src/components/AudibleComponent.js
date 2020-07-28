0d924e13977c1b5ada7c2a436643095c302d9f85
Sourabhtripathi <sourabh.tripathi48@gmail.com>
2020-07-28 19:31:08 +0530

    try out sine wave

--- src/components/AudibleComponent.js
+++ src/components/AudibleComponent.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import { useAudioPlayer } from 'react-use-audio-player'
+import React from 'react';
+import { useAudioPlayer } from 'react-use-audio-player';
 
 const AudibleComponent = ({ children, src }) => {
 	const { togglePlayPause, ready, loading, playing } = useAudioPlayer({
 		src,
 		format: 'mp3',
 		autoplay: false
-	})
-	return <div onClick={togglePlayPause}>{children}</div>
-}
+	});
+	return (
+		<div
+			onClick={() => {
+				console.log('clicked');
+				togglePlayPause();
+			}}
+		>
+			{children}
+		</div>
+	);
+};
 
-export default AudibleComponent
+export default AudibleComponent;
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,27 +1,174 @@
-import React, { Fragment, useState, useEffect } from 'react'
-import { Link, useParams, useLocation } from 'react-router-dom'
-import { IonIcon, IonButton, IonCard, IonGrid, IonRow, IonCol, IonContent } from '@ionic/react'
+import React, { Fragment, useState, useEffect, useRef } from 'react';
+import { Link, useParams, useLocation } from 'react-router-dom';
+import { IonIcon, IonButton, IonCard, IonGrid, IonRow, IonCol, IonContent } from '@ionic/react';
 
-import gmApi from '../apis/gmApi'
-import AccessibilityButton from './AccessibilityButton'
-import AudibleComponent from './AudibleComponent'
+import gmApi from '../apis/gmApi';
+import AccessibilityButton from './AccessibilityButton';
+import AudibleComponent from './AudibleComponent';
+import SineWaves from 'sine-waves';
 
-const colors = ['danger', 'success', 'primary', 'warning', 'secondary', 'tertiary', 'light', 'medium', 'dark']
+const colors = [ 'danger', 'success', 'primary', 'warning', 'secondary', 'tertiary', 'light', 'medium', 'dark' ];
 
 const CardList = () => {
-	const [cards, setCards] = useState([])
-	const params = useParams()
-	const location = useLocation()
+	const [ cards, setCards ] = useState([]);
+	const params = useParams();
+	const location = useLocation();
+	const refContainer = useRef();
 
-	console.log(params)
+	console.log(params);
+
 	useEffect(() => {
-		const { category = null, subcategory = null } = params
+		const { category = null, subcategory = null } = params;
 		if (!category && !subcategory) {
 			gmApi.get('/categories').then((response) => {
-				setCards(response.data)
-			})
+				setCards(response.data);
+			});
 		}
-	}, [])
+	}, []);
+
+	const onStop = () => {
+		var waves = new SineWaves({
+			// Canvas Element
+			el: refContainer.current,
+
+			speed: 0,
+
+			ease: 'SineInOut',
+
+			wavesWidth: '75%',
+
+			waves: [
+				{
+					timeModifier: 4,
+					lineWidth: 1,
+					amplitude: 0,
+					wavelength: 25
+				},
+				{
+					timeModifier: 2,
+					lineWidth: 1,
+					amplitude: 0,
+					wavelength: 30
+				},
+				{
+					timeModifier: 1,
+					lineWidth: 1,
+					amplitude: 0,
+					wavelength: 30
+				},
+				{
+					timeModifier: 3,
+					lineWidth: 1,
+					amplitude: 0,
+					wavelength: 40
+				},
+				{
+					timeModifier: 0.5,
+					lineWidth: 1,
+					amplitude: 0,
+					wavelength: 60
+				},
+				{
+					timeModifier: 1.3,
+					lineWidth: 1,
+					amplitude: 0,
+					wavelength: 40
+				}
+			],
+
+			// Called on window resize
+			resizeEvent: function() {
+				var gradient = this.ctx.createLinearGradient(0, 0, this.width, 0);
+				gradient.addColorStop(0, 'rgba(25, 255, 255, 0)');
+				gradient.addColorStop(0.5, 'rgba(255, 25, 255, 0.75)');
+				gradient.addColorStop(1, 'rgba(255, 255, 25, 0');
+
+				var index = -1;
+				var length = this.waves.length;
+				while (++index < length) {
+					this.waves[index].strokeStyle = gradient;
+				}
+
+				// Clean Up
+				index = void 0;
+				length = void 0;
+				gradient = void 0;
+			}
+		});
+		console.log(waves.ctx);
+	};
+
+	const onStart = () => {
+		var waves = new SineWaves({
+			// Canvas Element
+			el: refContainer.current,
+
+			speed: 8,
+
+			ease: 'SineInOut',
+
+			wavesWidth: '50%',
+
+			waves: [
+				{
+					timeModifier: 4,
+					lineWidth: 1,
+					amplitude: -25,
+					wavelength: 25
+				},
+				{
+					timeModifier: 2,
+					lineWidth: 1,
+					amplitude: -10,
+					wavelength: 30
+				},
+				{
+					timeModifier: 1,
+					lineWidth: 1,
+					amplitude: -30,
+					wavelength: 30
+				},
+				{
+					timeModifier: 3,
+					lineWidth: 1,
+					amplitude: 40,
+					wavelength: 40
+				},
+				{
+					timeModifier: 0.5,
+					lineWidth: 1,
+					amplitude: -60,
+					wavelength: 60
+				},
+				{
+					timeModifier: 1.3,
+					lineWidth: 1,
+					amplitude: -40,
+					wavelength: 40
+				}
+			],
+
+			// Called on window resize
+			resizeEvent: function() {
+				var gradient = this.ctx.createLinearGradient(0, 0, this.width, 0);
+				gradient.addColorStop(0, 'rgba(25, 255, 255, 0)');
+				gradient.addColorStop(0.5, 'rgba(255, 25, 255, 0.75)');
+				gradient.addColorStop(1, 'rgba(255, 255, 25, 0');
+
+				var index = -1;
+				var length = this.waves.length;
+				while (++index < length) {
+					this.waves[index].strokeStyle = gradient;
+				}
+
+				// Clean Up
+				index = void 0;
+				length = void 0;
+				gradient = void 0;
+			}
+		});
+		console.log(waves.ctx);
+	};
 
 	return (
 		<Fragment>
@@ -43,7 +190,10 @@ const CardList = () => {
 										<strong>{card.display_name}</strong>
 									</IonCol>
 									<IonCol size="3">
-										<Link to={`${location.pathname}/${card.keyword}/`} onClick={(e) => e.stopPropagation()}>
+										<Link
+											to={`${location.pathname}/${card.keyword}/`}
+											onClick={(e) => e.stopPropagation()}
+										>
 											<div style={{ color: 'white' }}>
 												<IonButton color={`${colors[index]}`} size="large">
 													&gt;
@@ -56,9 +206,14 @@ const CardList = () => {
 						</IonCard>
 					</AudibleComponent>
 				))}
+				<div className="container">
+					<button onClick={onStart}>Start</button>
+					<button onClick={onStop}>Stop</button>
+					<canvas className="waves" ref={refContainer} />
+				</div>
 			</IonContent>
 		</Fragment>
-	)
-}
+	);
+};
 
-export default CardList
+export default CardList;
