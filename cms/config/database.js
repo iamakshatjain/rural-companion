2dd7d392cf435c47e26a938f2b3b9b54f34a9f0a
iamakshatjain <jain99akshat@gmail.com>
2020-07-28 15:52:35 +0530

    feat: upload provider cloudinary

--- cms/config/database.js
+++ cms/config/database.js
@@ -4,8 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        uri:
-          "mongodb://admin_rc:admin_rc@gramin-mitra-shard-00-00.tfksc.mongodb.net:27017,gramin-mitra-shard-00-01.tfksc.mongodb.net:27017,gramin-mitra-shard-00-02.tfksc.mongodb.net:27017/gramin-mitra?ssl=true&replicaSet=atlas-ks6zw1-shard-0&authSource=admin&retryWrites=true&w=majority",
+        uri: env("DATABASE_URI"),
       },
       options: {
         ssl: true,
