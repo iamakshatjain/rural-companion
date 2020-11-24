166e1984ab7c328ae12769aecef9dddf9e8f1571
Subhajit Nandi <subhajit.nandi@innovaccer.com>
2020-11-24 23:26:29 +0530

    refactor: move redirects folder

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
-import { SET_AUDIO_SRC, SET_DEVICE } from './types'
+import { SET_AUDIO_SRC, SET_DEVICE } from './types';
 
 export const setAudioSrc = (src) => (dispatch) => {
-	dispatch({ type: SET_AUDIO_SRC, payload: src })
-}
+  dispatch({ type: SET_AUDIO_SRC, payload: src });
+};
 
 // set Device
 export const setDevice = (data) => (dispatch) => {
-	dispatch({ type: SET_DEVICE, payload: data })
-}
+  dispatch({ type: SET_DEVICE, payload: data });
+};
